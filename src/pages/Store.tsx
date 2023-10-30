0bef407bebe0cdaeb325f6ce5028017978efaed0
chethanavirajini <cheth.virajini@gmail.com>
2023-10-30 22:38:33 +0530

    Store Cards

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,6 +1,22 @@
+import { Col, Row } from "react-bootstrap"
+import storeItems from "../data/items.json"
+import  StoreItems  from "../components/StoreItems"
+
 
 export default function Store(){
     return(
-        <div>Store</div>
+        <>
+            <h1 className="text-center mb-4">Store</h1>
+            <Row md={2} xs={1} lg={3} className="g-3">
+                {
+                    storeItems.map((item)=>(
+                        <Col key={item.id}>
+                           <StoreItems {...item} />                        
+                        </Col>
+                    ))
+                }
+               
+            </Row>
+        </>
     )
 }
